fed123b1a68a215841a14a350117c19bfbce3fa4
IKS-cod <ik.smirnov@yandex.ru>
2024-09-22 10:56:36 +0300

    Initial commit

---
+++ src/main/java/TWSp/TwSpApplication.java
@@ -0,0 +1,13 @@
+package TWSp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwSpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TWSp/TwSpApplicationTests.java
@@ -0,0 +1,13 @@
+package TWSp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwSpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
